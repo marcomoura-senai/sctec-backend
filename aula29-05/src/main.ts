d8e80802e766620f9dbba09de0c696e2361bc18a
marcomoura-senai <marco.moura@edu.sc.senai.br>
2026-05-30 20:02:02 -0300

    (aula29_05) chore: add aula 29_05

---
+++ aula29-05/src/main.ts
@@ -0,0 +1,13 @@
+// eslint -> regras de boas práticas de programação // Code smells
+// prettier
+// editorconfig -> LF\ CRLF || 2 || 4 ||
+
+function main() {
+  const a = '';
+  const b = '';
+
+  console.log(a);
+  console.log(b);
+}
+
+main();
