8ee3cf1132729885471c7f295a2d89e602eb9037
marcomoura-senai <marco.moura@edu.sc.senai.br>
2026-06-29 13:10:56 -0300

    chore(loja-virtual): remove comments

--- postgres/loja-virtual/dql/cliente_sem_pedidos.sql
+++ postgres/loja-virtual/dql/cliente_sem_pedidos.sql
@@ -1,6 +1,3 @@
--- 1. Quantos pedidos cada cliente fez e quanto cada um gastou no total?
--- 2. Quais clientes fizeram mais de 2 pedidos?
--- 3. Liste os pedidos com o nome do cliente e o nome de cada produto.
 
 SELECT u.id, u.login, p.id as id_pedido, p.status FROM usuario u
 LEFT JOIN pedido p ON u.id = p.usuario_id
